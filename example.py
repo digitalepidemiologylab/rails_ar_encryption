3b8ea87771104b587a1906b76bf2154d390fce73
ghn <germain@ghmail.ch>
2022-04-22 17:26:13 +0200

    linter

--- example.py
+++ example.py
@@ -3,7 +3,10 @@ from rails_ar_encryption.base import derive_key, decrypt
 # RUN TEST
 password = "ptJvigCRrrlKX29kU4bx80FUmxmjUej0"
 salt = "dMoxVXAxoRrnOutfiPyeYqtfMiFICTva"
-message = {"p":"SY0Ky7wFH3i6s73Bi7LL2cI=","h":{"iv":"mr7kZJHH2DOYZDM6","at":"qlrSLDRNH0qpB9/ATlHbQg=="}}
+message = {
+    "p": "SY0Ky7wFH3i6s73Bi7LL2cI=",
+    "h": {"iv": "mr7kZJHH2DOYZDM6", "at": "qlrSLDRNH0qpB9/ATlHbQg=="},
+}
 
 key = derive_key(password, salt)
 clear_text = decrypt(message, key)
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding = "utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-   name="rails-ar-encryption",
-   version="0.0.1",
-   author="ghn",
-   description="Decrypts Rails payload.",
-   long_description = long_description,
-   long_description_content_type = "text/markdown",
-   packages=find_packages(exclude=['tests*']),
-   include_package_data=True,
-   install_requires=["pycryptodome"],
-   classifiers=[
-       "Programming Language :: Python :: 3",
-   ],
-   python_requires='>=3.6'
+    name="rails-ar-encryption",
+    version="0.0.1",
+    author="ghn",
+    description="Decrypts Rails payload.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    install_requires=["pycryptodome"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
 )
